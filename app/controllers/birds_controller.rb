306d09ae38d274d88d27c304316991999301e471
Gerald Bivens <geraldbivens@gmail.com>
2021-11-09 06:53:58 -0700

    Add index and show actions to the birds controller.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,2 +1,13 @@
 class BirdsController < ApplicationController
+
+    def index
+        @birds = Bird.all
+        render json: @birds
+    end
+
+    def show
+        @bird = Bird.find(params[:id])
+        render json: @bird
+    end
+
 end
