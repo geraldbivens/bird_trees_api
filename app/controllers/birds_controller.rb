f72bd77d9c18b90ab6aecaa2282bd2edfd0428e9
Gerald Bivens <geraldbivens@gmail.com>
2021-11-09 08:13:01 -0700

    Add status codes to birds controller actions.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -2,12 +2,12 @@ class BirdsController < ApplicationController
 
     def index
         @birds = Bird.all
-        render json: @birds
+        render json: @birds, status: :ok
     end
 
     def show
         @bird = Bird.find(params[:id])
-        render json: @bird
+        render json: @bird, status: :ok
     end
 
     def create
@@ -15,7 +15,7 @@ class BirdsController < ApplicationController
             name: params[:name],
             species: params[:species]
         )
-        render json: @bird
+        render json: @bird, status: :created
     end
 
 end
